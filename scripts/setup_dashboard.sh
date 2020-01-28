722c15e2aca0b053976aafa70f73fe35cd63b9bf
Samir Faci <samir@es.net>
2020-01-28 09:22:47 -1000

    [TechDebt] Creating a base image of vagrant
    ChangeLog:
      - Updated Vagrantfile to use the base image.
      - Created a workflow to update the base image grafana version

--- scripts/setup_dashboard.sh
+++ scripts/setup_dashboard.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
-## This script is invoked from the Vagrant file and can also be invoked on any RHEL server to perform a similar setup.
-yum install -y epel-release
-yum clean all
-yum install -y gcc\
-    kernel-devel\
-    kernel-headers\
-    dkms\
-    make\
-    bzip2\
-    perl\
-    perl-devel\
-    net-tools\
-    nc \
-    git\
-    npm\
-    rpm-build\
-    gcc-c++\
-    https://dl.grafana.com/oss/release/grafana-6.3.5-1.x86_64.rpm
-
 #Install wizzy
 npm install -g wizzy@0.6.0
 
